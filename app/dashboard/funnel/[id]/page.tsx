412cbfa4c7a167d26ca1954e2275ee70ec68829e
skrodrigo <rodrigoa0987@gmail.com>
2025-10-12 14:04:44 -0300

    feat: add widgets

--- app/dashboard/funnel/[id]/page.tsx
+++ app/dashboard/funnel/[id]/page.tsx
@@ -1,20 +1,67 @@
 "use client";
 
 import {
+  IconAlertTriangle,
+  IconArrowsShuffle,
+  IconChartArcs3,
+  IconChartBar,
+  IconCircuitPushbutton,
+  IconClock,
+  IconDeviceAudioTape,
   IconDeviceMobile,
+  IconGripHorizontal,
+  IconHelp,
+  IconLoader,
+  IconMessageCircle,
   IconPlayerPlay,
+  IconPlus,
+  IconPrinter,
+  IconRuler2,
+  IconScale,
   IconShare2,
+  IconSpacingVertical,
+  IconStar,
+  IconTag,
+  IconTypography,
+  IconVideo,
 } from "@tabler/icons-react";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 import { useQueryState } from "nuqs";
 import { Button } from "@/components/ui/button";
 import { ButtonGroup } from "@/components/ui/button-group";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const widgets = [
+  { id: "alerta", name: "Alerta", icon: IconAlertTriangle },
+  { id: "altura", name: "Altura", icon: IconRuler2 },
+  { id: "argumentos", name: "Argumentos", icon: IconChartArcs3 },
+  { id: "audio", name: "Áudio", icon: IconDeviceAudioTape },
+  { id: "botao", name: "Botão", icon: IconCircuitPushbutton },
+  { id: "captura", name: "Captura", icon: IconPrinter },
+  { id: "carrosel", name: "Carrosel", icon: IconArrowsShuffle },
+  { id: "depoimentos", name: "Depoimentos", icon: IconMessageCircle },
+  { id: "espaco", name: "Espaço", icon: IconSpacingVertical },
+  { id: "faq", name: "FAQ", icon: IconHelp },
+  { id: "graficos", name: "Gráficos", icon: IconChartBar },
+  { id: "loading", name: "Loading", icon: IconLoader },
+  { id: "metricas", name: "Métricas", icon: IconChartBar },
+  { id: "nivel", name: "Nível", icon: IconStar },
+  { id: "notificacao", name: "Notificação", icon: IconAlertTriangle },
+  { id: "opcao", name: "Opção", icon: IconCircuitPushbutton },
+  { id: "peso", name: "Peso", icon: IconScale },
+  { id: "preco", name: "Preço", icon: IconTag },
+  { id: "texto", name: "Texto", icon: IconTypography },
+  { id: "timer", name: "Timer", icon: IconClock },
+  { id: "video", name: "Vídeo", icon: IconVideo },
+];
 
 export default function Page() {
   const [activeTab, setActiveTab] = useQueryState("tab", {
     defaultValue: "funis",
   });
+  const { theme } = useTheme();
   return (
     <div className="h-screen flex flex-col">
       <Tabs
@@ -23,11 +70,11 @@ export default function Page() {
         className="flex flex-col flex-1"
       >
         <header className="bg-background border-b">
-          <div className="w-full mx-auto px-4 sm:px-6">
+          <div className="w-full mx-auto px-2">
             <div className="flex items-center gap-2 sm:gap-4 min-h-12 max-h-12">
               <div className="flex items-center gap-2 sm:gap-4">
                 <Image
-                  src="/quizy.svg"
+                  src={theme === "dark" ? "/quizy.svg" : "/quizy-black.svg"}
                   alt="Quizy"
                   width={100}
                   height={100}
@@ -58,16 +105,15 @@ export default function Page() {
               </div>
 
               <div className="flex items-center gap-1 sm:gap-2 ml-auto">
-
                 <ButtonGroup>
-                  <Button variant="outline" size='icon'>
-                    <IconShare2 />
+                  <Button variant="outline" size="icon">
+                    <IconShare2 stroke={1.2} />
                   </Button>
-                  <Button variant="outline" size='icon'>
-                    <IconDeviceMobile />
+                  <Button variant="outline" size="icon">
+                    <IconDeviceMobile stroke={1.2} />
                   </Button>
-                  <Button variant="outline" size='icon'>
-                    <IconPlayerPlay />
+                  <Button variant="outline" size="icon">
+                    <IconPlayerPlay stroke={1.2} />
                   </Button>
                 </ButtonGroup>
 
@@ -90,13 +136,52 @@ export default function Page() {
           </div>
         </header>
 
-        <main className="flex w-full px-4 flex-1 overflow-hidden sr-only">
-          <div className="bg-muted/60 min-w-[340px] rounded-xl m-2 p-2">
-            aside esquerdo
+        <main className="flex w-full flex-1 overflow-hidden ">
+          <div className="border-r h-full p-1 min-w-[200px] max-w-[200px] space-y-2">
+            <Button variant="outline" className="w-full justify-start">
+              <IconGripHorizontal stroke={1.2} />
+              <span className="text-muted-foreground">Etapa 1</span>
+            </Button>
+            <Button variant="outline" className="w-full justify-start">
+              <IconGripHorizontal stroke={1.2} />
+              <span className="text-muted-foreground">Etapa 2</span>
+            </Button>
+            <Button variant="outline" className="w-full justify-start">
+              <IconGripHorizontal stroke={1.2} />
+              <span className="text-muted-foreground">Etapa 3</span>
+            </Button>
+            <Button variant="link" className="w-full justify-center">
+              <IconPlus stroke={1.2} />
+              <span className="text-foreground">Adicionar Etapa</span>
+            </Button>
           </div>
-          <div className="flex-1 items-center justify-center">meio</div>
-          <div className="bg-muted/60 min-w-[340px] rounded-xl m-2 p-2">
-            aside direito
+          <ScrollArea className="max-h-[calc(100vh-48px)]">
+            <div className="border-r h-full p-1 min-w-[200px] max-w-[200px] space-y-2">
+              {widgets.map((widget) => {
+                const Icon = widget.icon;
+                return (
+                  <Button
+                    key={widget.id}
+                    variant="outline"
+                    className="w-full justify-start"
+                  >
+                    <Icon stroke={1.2} />
+                    <span className="text-foreground">{widget.name}</span>
+                  </Button>
+                );
+              })}
+            </div>
+          </ScrollArea>
+          <div className="flex-1 flex justify-center items-center">
+            <span>meio</span>
+          </div>
+          <div className="border-l h-full">
+            <div className="w-full border-b">
+              <div className="flex gap-2 p-2">
+                <Button variant="ghost">Componente</Button>
+                <Button variant="ghost">Estilo</Button>
+              </div>
+            </div>
           </div>
         </main>
       </Tabs>
